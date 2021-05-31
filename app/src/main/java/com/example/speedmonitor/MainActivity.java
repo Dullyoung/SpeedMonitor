bf944c6801be3e54ec27f8b9c533f1fdedfcb3b7
Dullyoung <664846453@qq.com>
2021-05-31 14:42:05 +0800

    use Gps first

--- app/src/main/java/com/example/speedmonitor/MainActivity.java
+++ app/src/main/java/com/example/speedmonitor/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.speedmonitor;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
@@ -17,14 +25,21 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private String TAG = "aaaaa";
     LocationManager mLocationManager;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_msg);
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        init();
+    }
+
+    private void init() {
         PermissionHelper permissionHelper = new PermissionHelper();
-        permissionHelper.setMustPermissions2(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION);
+        permissionHelper.setMustPermissions2(Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
         permissionHelper.checkAndRequestPermission(this, new PermissionHelper.OnRequestPermissionsCallback() {
             @SuppressLint("MissingPermission")
             @Override
@@ -32,20 +47,25 @@ public class MainActivity extends AppCompatActivity {
                 List<String> providers = mLocationManager.getProviders(true);
                 Log.i(TAG, "initViews: " + providers.toString());
                 String locationProvider;
-                if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
-                    //如果是网络定位
-                    Log.d(TAG, "如果是网络定位");
-                    locationProvider = LocationManager.NETWORK_PROVIDER;
-                } else if (providers.contains(LocationManager.GPS_PROVIDER)) {
+                if (providers.contains(LocationManager.GPS_PROVIDER)) {
                     //如果是GPS定位
                     Log.d(TAG, "如果是GPS定位");
                     locationProvider = LocationManager.GPS_PROVIDER;
+                } else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+                    //如果是网络定位
+                    Log.d(TAG, "如果是网络定位");
+                    locationProvider = LocationManager.NETWORK_PROVIDER;
                 } else {
                     Log.d(TAG, "没有可用的位置提供器");
 
+                    textView.setText("没有可用的位置提供器");
+                    Toast.makeText(MainActivity.this, "没有可用的位置提供器", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivityForResult(intent, 777);
                     return;
                 }
-                mLocationManager.requestLocationUpdates(locationProvider, 100, 0, new LocationListener() {
+                textView.setText("正在使用" + locationProvider + "定位");
+                mLocationManager.requestLocationUpdates(locationProvider, 0, 0, new LocationListener() {
 
                     /**
                      * 当某个位置提供者的状态发生改变时
@@ -93,7 +113,6 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             speed = distance / (time * 1000) + " mm/s";
                         }
-                        TextView textView = findViewById(R.id.tv_msg);
                         String text = "上次时间:" + lastTime +
                                 "两次时间间隔" + time +
                                 "\n本次纬度:" + location.getLatitude() +
@@ -101,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
                                 "\n本次经度:" + location.getLongitude() +
                                 "\n上次经度:" + lastLongitude +
                                 "\n速度：" + speed +
+                                "\n定位方式：" + locationProvider +
                                 "\n精度" + location.getAccuracy();
                         textView.setText(text);
                         setUI(speed);
@@ -112,8 +132,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onRequestPermissionError() {
                 Log.i(TAG, "onRequestPermissionError: ");
+                Toast.makeText(MainActivity.this, "请打开相关权限", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 888);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 777 || requestCode == 888) {
+            Log.i(TAG, "onActivityResult: " + requestCode + "---" + resultCode);
+            init();
+        }
     }
 
     private void setUI(String speed) {
